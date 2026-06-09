eae89dc481f390b3c9af3dbe4a290e8f35176807
Isabela <isabelalucind@gmail.com>
2026-06-09 11:03:48 -0300

    feat: criando array de objetos

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const acoes = [{ 
+   icone: "📚",
+    titulo: "Educar e Conscientizar",
+    descricao: "Organizar palestras, workshops e campanhas de conscientização para informar a comunidade sobre os benefícios da energia solar e como ela pode ser adotada em residências e empresas."
+}, {
+    icone: "💡",
+    titulo: "Incentivar a Instalação de Painéis Solares",
+    descricao: "Promover programas de incentivo para a instalação de painéis solares, como descontos, financiamentos ou parcerias com empresas de energia solar para facilitar o acesso à tecnologia."
+}, {
+    icone: "🏛️",
+    titulo: "Apoiar Políticas Públicas",
+    descricao: "Engajar-se com representantes locais para apoiar políticas públicas que incentivem o uso de energia renovável, incluindo subsídios para energia solar e regulamentações favoráveis."
+}, {
+    icone: "🏘️",
+    titulo: "Participar de Projetos Comunitários",
+    descricao: "Participar ou organizar projetos comunitários de energia solar, como a instalação de painéis solares em escolas, centros comunitários ou áreas públicas para demonstrar os benefícios da tecnologia."
+}]
+
+console.log(acoes[0])
